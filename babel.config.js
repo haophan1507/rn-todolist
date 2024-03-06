4677dbbfccca06b7730e9a58e4b46af3295a2f54
Hao Phan <phanvanhao1507@gmail.com>
2024-03-06 10:00:22 +0700

    feat: re init project

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,3 @@
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-      // Required for expo-router
-      'expo-router/babel',
-    ],
-    env: {
-      production: {
-        plugins: ['react-native-paper/babel'],
-      },
-    },
-  };
+module.exports = {
+  presets: ['module:@react-native/babel-preset'],
 };
